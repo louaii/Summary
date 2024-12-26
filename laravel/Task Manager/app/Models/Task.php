98a3ee8efc1db4c19a4926222b8d87f1aaea3df7
louaii <serhallouay2@gmail.com>
2024-12-26 19:38:12 +0200

    Model

---
+++ laravel/Task Manager/app/Models/Task.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+//database through tables routed by eloquent ORM
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    use HasFactory;
+}
