ec1b4ae582ce55cd43782bd7e7a5062fb7be7375
louaii <serhallouay2@gmail.com>
2025-01-02 16:47:25 +0200

    Seeder

--- laravel/Task Manager/database/seeders/CategorySeeder.php
+++ laravel/Task Manager/database/seeders/CategorySeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -10,8 +11,12 @@ class CategorySeeder extends Seeder
     /**
      * Run the database seeds.
      */
+    //to add these categories even when deleting data from database
+    //send it through php artisan db:seed --class=CategorySeeder
     public function run(): void
     {
-        //
+        $categories=['Work', 'Personal', 'Projects', 'Education', 'Finance', 'Health', 'Fitness'];
+        foreach($categories as $category)
+            Category::create(['name'=>$category]);
     }
 }
