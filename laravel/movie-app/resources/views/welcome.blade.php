26ab58854345afca0cd0715016c87302ff9388d8
louaii <serhallouay2@gmail.com>
2025-01-20 20:48:31 +0200

    12:12

--- laravel/movie-app/resources/views/welcome.blade.php
+++ laravel/movie-app/resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Laravel</title>
-        <link rel="stylesheet" href="css/app.css">
+        <link rel="stylesheet" href="/css/app.css">
     </head>
     <body class="font-sans antialiased dark:bg-black dark:text-white/50">
         <div class="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
--- laravel/movie-app/routes/web.php
+++ laravel/movie-app/routes/web.php
@@ -3,5 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+Route::get('/movie', function () {
+    return view('show');
+});
+
+// Route::view('/', 'index');
