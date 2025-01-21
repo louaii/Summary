4dbb706f03b3175b87e73a2373b327345fc38ac0
louaii <serhallouay2@gmail.com>
2025-01-21 19:37:51 +0200

    route/web.php

--- laravel/movie-app/routes/web.php
+++ laravel/movie-app/routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+//edit pages here not edited yet
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,4 +10,15 @@ Route::get('/movie', function () {
     return view('show');
 });
 
-// Route::view('/', 'index');
+
+
+// Route::get('/', 'MoviesController@index')->name('movies.index');
+// Route::get('/movies/{id}', 'MoviesController@show')->name('movies.show');
+
+// Route::get('/tv', 'TvController@index')->name('tv.index');
+// Route::get('/tv/{id}', 'TvController@show')->name('tv.show');
+
+// Route::get('/actors', 'ActorsController@index')->name('actors.index');
+// Route::get('/actors/page/{page?}', 'ActorsController@index');
+
+// Route::get('/actors/{id}', 'ActorsController@show')->name('actors.show');
