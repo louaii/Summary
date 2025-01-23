6f96dd360c649daef30f30d370347b19908e70bb
louaii <serhallouay2@gmail.com>
2025-01-23 15:49:31 +0200

    MovieController

--- laravel/movie-app/app/Http/Controllers/MovieController.php
+++ laravel/movie-app/app/Http/Controllers/MovieController.php
@@ -14,31 +14,18 @@ class MovieController extends Controller
         $popularMovies = $this->fetchFromTmdb('movie/popular')['results'];
         $nowPlayingMovies = $this->fetchFromTmdb('movie/now_playing')['results'];
         $genresArray = $this->fetchFromTmdb('genre/movie/list')['genres'];
-
-        $genres = collect($genresArray)->mapWithKeys(function ($genre) {
-            return [$genre['id'] => $genre['name']];
-        });
-
-        return view('index', [
-            'popularMovies' => $popularMovies,
-            'nowPlayingMovies' => $nowPlayingMovies,
-            'genres' => $genres,
-        ]);
+        $genres = collect($genresArray)->mapWithKeys(function ($genre) {return [$genre['id'] => $genre['name']];});
+        return view('index', ['popularMovies' => $popularMovies, 'nowPlayingMovies' => $nowPlayingMovies, 'genres' => $genres,]);
     }
 
     public function show($id)
     {
         $movie = $this->fetchFromTmdb("movie/{$id}?append_to_response=credits,videos,images");
-
-        return view('show', [
-            'movie' => $movie,
-        ]);
+        return view('show', ['movie' => $movie,]);
     }
 
     private function fetchFromTmdb(string $endpoint)
     {
-        return Http::withToken($this->tmdbToken)
-            ->get("https://api.themoviedb.org/3/{$endpoint}")
-            ->json();
+        return Http::withToken($this->tmdbToken)->get("https://api.themoviedb.org/3/{$endpoint}")->json();
     }
 }
